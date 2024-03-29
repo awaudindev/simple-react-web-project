dd3ce87f08e31c0155c4c9c216f2e4e299d393b0
awaludin <awaludin.dev@gmail.com>
2024-03-29 17:13:04 +0700

    change repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Navbar } from "@/widgets/layout";
 import routes from "@/routes";
-import { Header } from "@/widgets/header";
-import { Footerr } from "@/widgets/footer";
-import './App.css'
+
 
 function App() {
   const { pathname } = useLocation();
 
   return (
     <>
-      <Header />
+      {!(pathname == '/sign-in' || pathname == '/sign-up') && (
+        <div className="container absolute left-2/4 z-10 mx-auto -translate-x-2/4 p-4">
+          <Navbar routes={routes} />
+        </div>
+      )
+      }
       <Routes>
         {routes.map(
           ({ path, element }, key) =>
             element && <Route key={key} exact path={path} element={element} />
         )}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
-      <Footerr />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
